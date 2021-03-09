7eb56e7335768887f219045c650f07d3330da422
Abhipsa1012 <abhipsaguru@gmail.com>
2021-03-09 16:59:26 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-import UseEffect from "./component/useEffect";
-import Parent from "./component/DataPass/Parent";
-import FocusBanner from "./component/FigmaCool/FocusBanner";
 import UI from "./component/UI/ui";
 
 function App() {
   return (
     <div className="App">
       <div>
-        {/* <UseEffect /> */}
-         {/* <Parent /> */}
-        {/* <FocusBanner /> */}
         <UI/>
       </div>
     </div>
